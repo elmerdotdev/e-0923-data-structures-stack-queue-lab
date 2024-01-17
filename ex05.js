e51eb7021ded646b9d73ebb281307c6d980076e4
Elmer Balbin <contact@elmer.dev>
2024-01-17 11:52:25 -0800

    updated ex05.js

--- ex05.js
+++ ex05.js
@@ -1,5 +1,7 @@
 // Using either the Stack or Queue principle, create a function that will reverse the position of the first element and the last element in the data structure
 // Example: [1, 2, 3, 4, 5] => [5, 2, 3, 4, 1]
+// Depending on which principle you prefer, make sure to import the corresponding class (Stack or Queue) before the function
+
 
 function reverseFirstAndLastElement() {
   // Your code here
